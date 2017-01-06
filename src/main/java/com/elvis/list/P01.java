7049f1a024b45d955e027b2219553edeb44e2882
elviswu <elviswu@aaxischina.com>
2017-01-06 10:56:24 +0800

    20170105

--- src/main/java/com/elvis/list/P01.java
+++ src/main/java/com/elvis/list/P01.java
@@ -1,6 +1,7 @@
 package com.elvis.list;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class P01 {
@@ -9,5 +10,20 @@ public class P01 {
             throw new NoSuchElementException();
         }
         return elements.getLast();
+    }
+    
+    public static <T> T last1(List<T> elements) {
+        return elements.get(elements.size()-1);
+    }
+    
+    public static <T> T lastRecursive(List<T> elements) {
+        if (elements == null || elements.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        if (elements.size() == 1) {
+            return elements.get(0);
+        }
+        
+        return lastRecursive(elements.subList(1, elements.size()));
     }
 }
--- src/test/java/com/elvis/list/P01Test.java
+++ src/test/java/com/elvis/list/P01Test.java
@@ -1,12 +1,13 @@
 package com.elvis.list;
 
+import static com.elvis.list.CollectionUtils.linkedList;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static java.util.Arrays.asList;
 
 import java.util.LinkedList;
-import static com.elvis.list.P01.last;
-import static com.elvis.list.CollectionUtils.linkedList;
+
 import org.junit.Test;
 
 public class P01Test{
@@ -14,5 +15,15 @@ public class P01Test{
     public void findLastElementsFromAListofAlphabets() {
         LinkedList<String> alphabets = linkedList("a","b","c","d");
         assertThat(P01.last(alphabets),is(equalTo("d")));
+    }
+    
+    @Test
+    public void findLast1ElementsFromAListofAlphabets() {
+        assertThat(P01.last1(asList("a", "b", "c", "d")), is(equalTo("d")));
+    }
+    
+    @Test
+    public void findLast2ElementsFromAListofAlphabets() {
+        assertThat(P01.lastRecursive(asList("a","b","c","d")), is(equalTo("d")));
     }
 }
