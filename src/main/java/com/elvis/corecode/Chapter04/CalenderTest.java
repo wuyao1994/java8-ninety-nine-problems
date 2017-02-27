2fd1710a715b08b71a8d09a8846ce1ee6345a576
elviswu <elviswu@aaxischina.com>
2017-02-27 18:35:31 +0800

    20170227-2

---
+++ src/main/java/com/elvis/corecode/Chapter04/CalenderTest.java
@@ -0,0 +1,52 @@
+package com.elvis.corecode.Chapter04;
+
+import java.text.DateFormatSymbols;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class CalenderTest {
+    public static void main(String[] args) {
+        GregorianCalendar d = new GregorianCalendar();
+        int today = d.get(Calendar.DAY_OF_MONTH);
+        int month = d.get(Calendar.MONTH);
+        
+        d.set(Calendar.DAY_OF_MONTH, 1);
+        int weekday = d.get(Calendar.DAY_OF_WEEK);
+        int firstDatOfWeek = d.getFirstDayOfWeek();
+        int indent = 0;
+        
+        while(weekday != firstDatOfWeek) {
+            indent++;
+            d.add(Calendar.DAY_OF_MONTH, -1);
+            weekday = d.get(Calendar.DAY_OF_WEEK);
+        }
+        String[] weekdayNames = new DateFormatSymbols().getShortWeekdays();
+        do {
+            System.out.printf("%4s", weekdayNames[weekday]);
+            d.add(Calendar.DAY_OF_MONTH, 1);
+            weekday = d.get(Calendar.DAY_OF_WEEK);
+        } while(weekday != firstDatOfWeek);
+        System.out.println();
+        for (int i = 1; i < indent; i++) {
+            System.out.print("  ");
+        }
+        d.set(Calendar.DAY_OF_MONTH, 1);
+        do {
+            int day = d.get(Calendar.DAY_OF_MONTH);
+            System.out.printf("%3d", day);
+            if (day== today) {
+                System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+            d.add(Calendar.DAY_OF_MONTH, 1);
+            weekday = d.get(Calendar.DAY_OF_WEEK);
+            if (weekday == firstDatOfWeek) {
+                System.out.println();
+            }
+        } while (d.get(Calendar.MONTH) == month);
+        if (weekday != firstDatOfWeek) {
+            System.out.println();
+        }
+    }
+}
