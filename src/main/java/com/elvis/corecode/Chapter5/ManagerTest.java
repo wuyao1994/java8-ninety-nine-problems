a8e430864c1f18b609cb3089f3049b11186c1a45
elviswu <elviswu@aaxischina.com>
2017-03-02 16:27:32 +0800

    20170302

---
+++ src/main/java/com/elvis/corecode/Chapter5/ManagerTest.java
@@ -0,0 +1,19 @@
+package com.elvis.corecode.Chapter5;
+
+public class ManagerTest {
+
+    public static void main(String[] args) {
+        Manager boss = new Manager("Carl Craker", 80000, 1987, 12, 15);
+        boss.setBouns(5000);
+        
+        Employee[] staff = new Employee[3];
+        staff[0] = boss;
+        staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
+        staff[2] = new Employee("Tommy Tester", 40000, 1990, 3, 15);
+        
+        for (Employee e : staff) {
+            System.out.println("name="+e.getName()+",salary="+e.getSalary()+",hireDay="+e.getHireDay());
+        }
+    }
+
+}
