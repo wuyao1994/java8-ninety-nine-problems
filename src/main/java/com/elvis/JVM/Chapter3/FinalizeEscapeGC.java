a4cb26a0b1b3b6376aef34ea58563dfcd46b6e5f
elvis <elvis@ip-192-168-199-225>
2017-03-06 23:26:25 +0800

    finalize escape gc

---
+++ src/main/java/com/elvis/JVM/Chapter3/FinalizeEscapeGC.java
@@ -0,0 +1,38 @@
+package com.elvis.JVM.Chapter3;
+
+public class FinalizeEscapeGC {
+    public static FinalizeEscapeGC SAVE_HOOK = null;
+    public void isAlive() {
+        System.out.println("yes, ian still alive :)");
+    }
+    
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("finalize method executed!");
+        FinalizeEscapeGC.SAVE_HOOK = this;
+        System.out.println(FinalizeEscapeGC.SAVE_HOOK);
+        System.out.println(this);
+    }
+
+    public static void main(String[] args) throws Exception{
+        SAVE_HOOK = new FinalizeEscapeGC();
+        SAVE_HOOK = null;
+        System.gc();
+        Thread.sleep(500);
+        if (SAVE_HOOK != null) {
+            SAVE_HOOK.isAlive();
+        } else {
+            System.out.println("no, i ma dead :(");
+        }
+        
+        SAVE_HOOK = null;
+        System.gc();
+        Thread.sleep(500);
+        if (SAVE_HOOK != null) {
+            SAVE_HOOK.isAlive();
+        } else {
+            System.out.println("no, i ma dead :(");
+        }
+    }
+}
