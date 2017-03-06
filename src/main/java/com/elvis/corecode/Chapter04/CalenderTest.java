1a70deb229a18bcb4c9e8f7592880790eae2927a
elvis <elvis@ip-192-168-199-225>
2017-03-06 21:04:25 +0800

    20170306

--- src/main/java/com/elvis/corecode/Chapter04/CalenderTest.java
+++ src/main/java/com/elvis/corecode/Chapter04/CalenderTest.java
@@ -11,24 +11,26 @@ public class CalenderTest {
         int month = d.get(Calendar.MONTH);
         
         d.set(Calendar.DAY_OF_MONTH, 1);
-        int weekday = d.get(Calendar.DAY_OF_WEEK);
-        int firstDatOfWeek = d.getFirstDayOfWeek();
+        int weekday = d.get(Calendar.DAY_OF_WEEK);//4
+        int firstDatOfWeek = d.getFirstDayOfWeek();//1
         int indent = 0;
         
         while(weekday != firstDatOfWeek) {
             indent++;
+            
             d.add(Calendar.DAY_OF_MONTH, -1);
             weekday = d.get(Calendar.DAY_OF_WEEK);
         }
         String[] weekdayNames = new DateFormatSymbols().getShortWeekdays();
+        
         do {
             System.out.printf("%4s", weekdayNames[weekday]);
             d.add(Calendar.DAY_OF_MONTH, 1);
             weekday = d.get(Calendar.DAY_OF_WEEK);
         } while(weekday != firstDatOfWeek);
-        System.out.println();
-        for (int i = 1; i < indent; i++) {
-            System.out.print("  ");
+        System.out.println();//换行
+        for (int i = 1; i <= indent; i++) {
+            System.out.print("    ");
         }
         d.set(Calendar.DAY_OF_MONTH, 1);
         do {
